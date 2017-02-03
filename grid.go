b0f79e89dcf25caf7fed265941f81d89dc5a66aa
Alok Menghrajani <alok@squareup.com>
2017-02-02 20:29:33 -0800

    Possibly slower but simpler implementation for partial words.
    This will result is less copying, so it might not even be slower at
    the end of the day. In either case, having simpler code is worth
    it.

--- grid.go
+++ grid.go
@@ -32,16 +32,23 @@ func (c emptyCell) String() string {
 
 type charCell struct {
 	char      byte
-	isPartial bool
+	isDown    bool
+	isRight   bool
 }
 
 func (c charCell) isCell() {}
 func (c charCell) String() string {
-	if c.isPartial {
-		return fmt.Sprintf("(%c)", c.char)
+	r := fmt.Sprintf(" %c", c.char);
+	if c.isDown && !c.isRight {
+		r += "|"
+	} else if !c.isDown && c.isRight {
+		r += "-"
+	} else if c.isDown && c.isRight {
+		r += "+"
 	} else {
-		return fmt.Sprintf(" %c ", c.char)
+		r = "WTF"
 	}
+	return r
 }
 
 type point struct {
@@ -51,16 +58,12 @@ type point struct {
 type Grid struct {
 	size int
 	grid [][]cell
-	rightPartial map[point]int
-	downPartial map[point]int
 }
 
 func New(size int) *Grid {
 	g := new(Grid)
 	g.size = size
 	g.grid = make([][]cell, size)
-	g.rightPartial = make(map[point]int)
-	g.downPartial = make(map[point]int)
 	for i := 0; i < size; i++ {
 		g.grid[i] = make([]cell, size)
 		for j := 0; j < size; j++ {
@@ -108,15 +111,6 @@ func (g *Grid) isEmptyOrLetter(x, y int, char byte) bool {
 	return false
 }
 
-func (g *Grid) removeRightPartial(x, y int) bool {
-	_, ok := g.rightPartial[point{x, y}]
-	if ok {
-		delete(g.rightPartial, point{x, y})
-		return true
-	}
-	return false
-}
-
 func (g *Grid) Place(x, y int, dir dir, word string) bool {
 	// we can place a word only if the cells before and after the word are empty or black
 	// and if every cell is empty or has the same value as the letter being placed.
@@ -146,68 +140,16 @@ func (g *Grid) placeDown(x, y int, word string) bool {
 		g.grid[x][y+len(word)] = blackCell{}
 	}
 	for i := 0; i < len(word); i++ {
-		if _, ok := g.grid[x][y+i].(charCell); ok {
-			g.grid[x][y+i] = charCell{char: word[i], isPartial: false}
-		} else if g.isEmptyOrBlack(x-1, y+i) && g.isEmptyOrBlack(x+1, y+i) {
-			g.grid[x][y+i] = charCell{char: word[i], isPartial: false}
+		if t, ok := g.grid[x][y+i].(charCell); ok {
+			t.isDown = true
+			g.grid[x][y+i] = t
 		} else {
-			// Since words are always surrounded by black cells, we only need
-			// to look at the cell to the right and left and mark it as partial.
-			// The whole partial tracking logic is currently very complicated and
-			// hard to correctly test. Perhaps I should look into a different way
-			// to track the state of the board. For each cell, I could store
-			// if it's part of a down or right word. Then scan the board and find
-			// partials that need to be fulfilled.
-			//
-			// Go all the way left. Look if it matches a partial. If it does, remove it
-			// Go one right. Look if it matches a partial. If it does, remove it.
-			// Add all the way left to all the way right as a new partial.
-			leftOldPartial := 0
-			if !g.isEmptyOrBlack(x-1, y+i) {
-				t := g.grid[x-1][y+i].(charCell)
-				t.isPartial = true
-				g.grid[x-1][y+i] = t
-				for j:=1; !g.isEmptyOrBlack(x-j, y+i); j++ {
-					leftOldPartial = j
-				}
-			}
-			if leftOldPartial > 1 {
-				for j:=1; j<leftOldPartial; j++ {
-					if g.removeRightPartial(x-j, y+i) {
-						panicIfFalse(false, fmt.Sprintf("wasn't expecting a partial at %d-%d,%d+%d", x, j, y, i));
-					}
-				}
-				if !g.removeRightPartial(x-leftOldPartial, y+i) {
-					panicIfFalse(false, fmt.Sprintf("was expecting a partial at %d-%d,%d+%d", x, leftOldPartial, y, i));
-				}
-			}
-			rightOldPartial := 0
-			if !g.isEmptyOrBlack(x+1, y+i) {
-				t := g.grid[x+1][y+i].(charCell)
-				t.isPartial = true
-				g.grid[x+1][y+i] = t
-				for j:=1; !g.isEmptyOrBlack(x+j, y+i); j++ {
-					rightOldPartial = j
-				}
-			}
-			if rightOldPartial > 1 {
-				for j:=1; j<rightOldPartial; j++ {
-					if g.removeRightPartial(x+j, y+i) {
-						panicIfFalse(false, fmt.Sprintf("wasn't expecting a partial at %d+%d,%d+%d", x, j, y, i));
-					}
-				}
-				if !g.removeRightPartial(x+rightOldPartial, y+i) {
-					panicIfFalse(false, fmt.Sprintf("was expecting a partial at %d+%d,%d+%d", x, rightOldPartial, y, i));
-				}
-			}
-			g.rightPartial[point{x-leftOldPartial, y+i}] = leftOldPartial + rightOldPartial + 1
-
 			// You might think that we can get away without using partial words
 			// (and having word insertion ordering solve the partial words issue)
 			// but then we'll fail to explore the entire search space because of
 			// "circular" partial words. I prefer to go through the words in a fixed
 			// order and deal with these partial words.
-			g.grid[x][y+i] = charCell{char: word[i], isPartial: true}
+			g.grid[x][y+i] = charCell{char: word[i], isDown: true, isRight: false}
 		}
 	}
 	return true
@@ -232,12 +174,11 @@ func (g *Grid) placeRight(x, y int, word string) bool {
 		g.grid[x+len(word)][y] = blackCell{}
 	}
 	for i := 0; i < len(word); i++ {
-		if _, ok := g.grid[x+i][y].(charCell); ok {
-			g.grid[x+i][y] = charCell{char: word[i], isPartial: false}
-		} else if g.isEmptyOrBlack(x, y-1) && g.isEmptyOrBlack(x, y+1) {
-			g.grid[x+i][y] = charCell{char: word[i], isPartial: false}
+		if t, ok := g.grid[x+i][y].(charCell); ok {
+			t.isRight = true
+			g.grid[x+i][y] = t
 		} else {
-			g.grid[x+i][y] = charCell{char: word[i], isPartial: true}
+			g.grid[x+i][y] = charCell{char: word[i], isDown: false, isRight: true}
 		}
 	}
 	return true
--- grid_test.go
+++ grid_test.go
@@ -16,24 +16,23 @@ func TestPlace(t *testing.T) {
 	g := New(4)
 	assert.True(t, g.Place(1, 1, DOWN, "foo"))
 	s := g.String()
-	assert.Equal(t, s, " .  #  .  . \n .  f  .  . \n .  o  .  . \n .  o  .  . \n")
+	assert.Equal(t, s, " .  #  .  . \n .  f| .  . \n .  o| .  . \n .  o| .  . \n")
 
 	assert.True(t, g.Place(0, 2, RIGHT, "dome"))
 	s = g.String()
-	assert.Equal(t, s, " .  #  .  . \n .  f  .  . \n d  o  m  e \n .  o  .  . \n")
+	assert.Equal(t, s, " .  #  .  . \n .  f| .  . \n d- o+ m- e-\n .  o| .  . \n")
 
 	assert.True(t, g.Place(3, 1, DOWN, "be"))
 	s = g.String()
-	assert.Equal(t, s, " .  #  .  # \n .  f  .  b \n d  o  m  e \n .  o  .  # \n")
+	assert.Equal(t, s, " .  #  .  # \n .  f| .  b|\n d- o+ m- e+\n .  o| .  # \n")
 
 	assert.True(t, g.Place(2, 0, DOWN, "sam"))
 	s = g.String()
-	assert.Equal(t, s, " .  #  s  # \n . (f)(a)(b)\n d  o  m  e \n .  o  #  # \n")
-
-	fmt.Println(g)
-	fmt.Printf("here: %s", g.rightPartial)	
+	assert.Equal(t, s, " .  #  s| # \n .  f| a| b|\n d- o+ m+ e+\n .  o| #  # \n")
 
 	assert.True(t, g.Place(1, 1, RIGHT, "fab"))
 	s = g.String()
-	assert.Equal(t, s, " .  #  s  # \n #  f  a  b \n d  o  m  e \n .  o  #  # \n")
+	assert.Equal(t, s, " .  #  s| # \n #  f+ a+ b+\n d- o+ m+ e+\n .  o| #  # \n")
+
+	fmt.Println(g)
 }
